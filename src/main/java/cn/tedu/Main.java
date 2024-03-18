00223e2da45741a39660438774314948e234e337
luchong <luchong@tedu.cn>
2024-03-18 10:07:36 +0800

    idea git commit03

--- src/main/java/cn/tedu/Main.java
+++ src/main/java/cn/tedu/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!222222222222222222222");
+        System.out.println("Hello world!333333333333333333333");
     }
 }
